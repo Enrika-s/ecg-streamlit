eff75186af6f2b1a625ce6d0f1cbd0a43632fd47
Enrika Soucy <souenrika@gmail.com>
2024-06-12 17:09:25 -0400

    reverting to non centered button

--- app.py
+++ app.py
@@ -49,11 +49,6 @@ def main():
         display: flex;
         justify-content: center;
     }
-    .button-container {
-        display: flex;
-        justify-content: center;
-        margin-top: 20px;
-    }
     </style>
     """, unsafe_allow_html=True)
 
@@ -67,9 +62,6 @@ def main():
     st.markdown("""
     Welcome to the ECG Classification App. This tool allows you to upload an ECG file in CSV format and get a classification prediction indicating whether the ECG is **Normal** or **Abnormal (Arrhythmia)**.
     """)
-
-    # Preload the model outside the spinner context
-    model, scaler = load_model()
 
     if "show_disclaimer" not in st.session_state:
         st.session_state.show_disclaimer = True
@@ -94,48 +86,47 @@ def main():
             with st.expander("Show Uploaded Data"):
                 st.write(user_data)
 
-            col1, col2, col3 = st.columns([1, 1, 1])
-            with col2:
-                if st.button('Analyze my results'):
-                    with st.spinner('Analyzing data... ❤️'):
-                        processed_data = preprocess_input(user_data, scaler)
-                        if processed_data is not None:
-                            prediction = model.predict(processed_data)
-                            prediction_proba = model.predict_proba(processed_data)
+            if st.button('Analyze my results'):
+                with st.spinner('Analyzing data... ❤️'):
+                    model, scaler = load_model()
+                    processed_data = preprocess_input(user_data, scaler)
+                    if processed_data is not None:
+                        prediction = model.predict(processed_data)
+                        prediction_proba = model.predict_proba(processed_data)
 
-                            prediction_label = 'Normal' if prediction[0] == 0 else 'Abnormal (Arrhythmia)'
-                            confidence_score = prediction_proba[0][prediction[0]] * 100
+                        prediction_label = 'Normal' if prediction[0] == 0 else 'Abnormal (Arrhythmia)'
+                        confidence_score = prediction_proba[0][prediction[0]] * 100
 
-                            st.write("## Prediction Result")
-                            st.markdown(f"<h3 style='text-align: center; color: {'green' if prediction[0] == 0 else 'red'};'>{prediction_label} ({confidence_score:.2f}% confidence)</h3>", unsafe_allow_html=True)
+                        st.write("## Prediction Result")
+                        st.markdown(f"<h3 style='text-align: center; color: {'green' if prediction[0] == 0 else 'red'};'>{prediction_label} ({confidence_score:.2f}% confidence)</h3>", unsafe_allow_html=True)
 
-                            if prediction[0] == 0:
-                                st.image("https://i.postimg.cc/nhMj7hpL/Normal.png", caption="ECG - Normal Sinus Rhythm", use_column_width=True)
-                                st.markdown("""
-                                ### What does this mean?
-                                **Normal**: The ECG data is classified as normal, indicating that the heart rhythm appears to be regular.
+                        if prediction[0] == 0:
+                            st.image("https://i.postimg.cc/nhMj7hpL/Normal.png", caption="ECG - Normal Sinus Rhythm", use_column_width=True)
+                            st.markdown("""
+                            ### What does this mean?
+                            **Normal**: The ECG data is classified as normal, indicating that the heart rhythm appears to be regular.
 
-                                **Explanation:**
-                                - A normal ECG shows a consistent rhythm and rate, with the heart beating at a regular interval.
+                            **Explanation:**
+                            - A normal ECG shows a consistent rhythm and rate, with the heart beating at a regular interval.
 
-                                **Note:**
-                                - While a normal ECG is a positive sign, it is essential to remember that this app provides information only and not a professional medical opinion.
-                                - If you have any concerns or symptoms, please consult a healthcare professional for a comprehensive evaluation.
-                                """)
-                            else:
-                                st.image("https://i.postimg.cc/br3rDK5b/Arrhythmia.png", caption="ECG - Abnormal (Arrhythmia)", use_column_width=True)
-                                st.markdown("""
-                                ### What does this mean?
-                                **Abnormal (Arrhythmia)**: The ECG data is classified as abnormal, indicating that there may be irregularities in the heart rhythm.
-                                - **Arrhythmia** refers to an irregular heart rhythm, which can be too fast, too slow, or erratic.
-                                - It is important to consult a healthcare professional for a detailed assessment and diagnosis.
-
-                                **Disclaimer:**
-                                - This app provides information only and is not a substitute for professional medical advice, diagnosis, or treatment.
-                                - Always seek the advice of your physician or other qualified health provider with any questions you may have regarding a medical condition.
-                                """)
+                            **Note:**
+                            - While a normal ECG is a positive sign, it is essential to remember that this app provides information only and not a professional medical opinion.
+                            - If you have any concerns or symptoms, please consult a healthcare professional for a comprehensive evaluation.
+                            """)
                         else:
-                            st.error("An error occurred during prediction: Invalid data format")
+                            st.image("https://i.postimg.cc/br3rDK5b/Arrhythmia.png", caption="ECG - Abnormal (Arrhythmia)", use_column_width=True)
+                            st.markdown("""
+                            ### What does this mean?
+                            **Abnormal (Arrhythmia)**: The ECG data is classified as abnormal, indicating that there may be irregularities in the heart rhythm.
+                            - **Arrhythmia** refers to an irregular heart rhythm, which can be too fast, too slow, or erratic.
+                            - It is important to consult a healthcare professional for a detailed assessment and diagnosis.
+
+                            **Disclaimer:**
+                            - This app provides information only and is not a substitute for professional medical advice, diagnosis, or treatment.
+                            - Always seek the advice of your physician or other qualified health provider with any questions you may have regarding a medical condition.
+                            """)
+                    else:
+                        st.error("An error occurred during prediction: Invalid data format")
 
     st.markdown("""
     <div class="emergency-text">
